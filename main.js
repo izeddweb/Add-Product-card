1f0806e7e8efa2bbd530f588b81d3a40784d2ab1
izedweb <abzedd61@gmail.com>
2023-03-30 00:37:24 +0100

    fix echo

--- main.js
+++ main.js
@@ -3,44 +3,54 @@
 const container = document.querySelector(".container");
 const input = document.querySelector("input");
 const buttonAdd = document.querySelector(".btn");
+var inputValue = 1;
 
-function AddingElement() {
-  let star = 1;
-  let end = 10;
-  for (let i = star; i <= end; i++) {
-    const mydiv = document.createElement("div");
-    const myPara = document.createElement("p");
-    const myLink = document.createElement("a");
-    const myTextPara = document.createTextNode(`Prod: ${i}`);
-    const myTextlink = document.createTextNode(`Link: ${i} `);
-    mydiv.appendChild(myTextPara);
-    mydiv.appendChild(myPara);
-    mydiv.appendChild(myLink);
-    myLink.appendChild(myTextlink);
-    myPara.appendChild(myTextPara);
-    myLink.setAttribute("class", "link");
-    mydiv.classList.add("styling-card");
-    container.appendChild(mydiv);
-  }
+function gettingValue() {
+  buttonAdd.onclick = function () {
+    inputValue = Number(input.value);
+    console.log(inputValue);
+
+    function addingElement() {
+      console.log(inputValue);
+
+      for (let item = 1; item <= inputValue; item++) {
+        const myPProd = document.createElement("div");
+        const textProd = document.createTextNode(`${item}-- textProd`);
+        console.log(textProd);
+        console.log(inputValue);
+        myPProd.appendChild(textProd);
+        myPProd.classList.add('styling-card')
+        // myPProd.appendChild(item);
+        container.appendChild(myPProd);
+      }
+    }
+    addingElement();
+  };
 }
-AddingElement();
+gettingValue();
 
-buttonAdd.onclick = function () {
-  console.log(inputValue);
-  inputValue = input.value;
-};
-
-// for(let i = 1; i<=10;i++){
-//   let myD = document.createElement("div");
-//   let myH= document.createElement("h3");
-//   let myP = document.createElement("p");
-//   let myText=document.createTextNode("product title" +[i] );
-//   let myT= document.createTextNode("paragraph discreption" +[i] );
-//   myD.className="prudoct";
-//   myD.classList.add('styling-card')
-//       myH.appendChild(myText);
-//       myP.appendChild(myT);
-//       myD.appendChild(myH);
-//       myD.appendChild(myP);
-// document.body.appendChild(myD);
+// function AddingElement() {
+//   let star = 1;
+//   let end = 10;
+//   for (let i = star; i <= end; i++) {
+//     const mydiv = document.createElement("div");
+//     const myPara = document.createElement("p");
+//     const myLink = document.createElement("a");
+//     const myTextPara = document.createTextNode(`Prod: ${i}`);
+//     const myTextlink = document.createTextNode(`Link: ${i} `);
+//     mydiv.appendChild(myTextPara);
+//     mydiv.appendChild(myPara);
+//     mydiv.appendChild(myLink);
+//     myLink.appendChild(myTextlink);
+//     myPara.appendChild(myTextPara);
+//     myLink.setAttribute("class", "link");
+//     mydiv.classList.add("styling-card");
+//     container.appendChild(mydiv);
+//   }
 // }
+// AddingElement();
+
+// buttonAdd.onclick = function () {
+//   console.log(inputValue);
+//   inputValue = input.value;
+// };
