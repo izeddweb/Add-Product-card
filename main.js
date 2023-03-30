ba7faca288e508c379327abbd22ce4abcc5b0d08
izedweb <abzedd61@gmail.com>
2023-03-30 14:07:46 +0100

    adding remainder file

--- main.js
+++ main.js
@@ -3,24 +3,24 @@
 const container = document.querySelector(".container");
 const input = document.querySelector("input");
 const buttonAdd = document.querySelector(".btn");
-// var inputValue = 1;
+var inputValue = 1;
 
-// function gettingValue() {
-//   buttonAdd.onclick = function () {
-//     inputValue = parseInt(input.value);
-//     // set function
-//     function addingElement() {
-//       for (let item = 1; item <= inputValue; item++) {
-//         const myDivProd = document.createElement("div");
-//         const textProd = document.createTextNode(`${item}-- textProd`);
-//         myDivProd.appendChild(textProd);
-//         myDivProd.classList.add("styling-card");
-//         container.appendChild(myDivProd);
-//       }
-//     }
-//     addingElement();
-//   };
-// }
-// gettingValue();
+function gettingValue() {
+  buttonAdd.onclick = function () {
+    inputValue = parseInt(input.value);
+    // set function
+    function addingElement() {
+      for (let item = 1; item <= inputValue; item++) {
+        const myDivProd = document.createElement("div");
+        const textProd = document.createTextNode(`${item}-- textProd`);
+        myDivProd.appendChild(textProd);
+        myDivProd.classList.add("styling-card");
+        container.appendChild(myDivProd);
+      }
+    }
+    addingElement();
+  };
+}
+gettingValue();
 
 
