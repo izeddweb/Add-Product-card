0103336b3bf9b7a6ac594ccf103123bf981e317c
izedweb <abzedd61@gmail.com>
2023-03-30 13:57:02 +0100

    clenning code 2

--- main.js
+++ main.js
@@ -3,54 +3,24 @@
 const container = document.querySelector(".container");
 const input = document.querySelector("input");
 const buttonAdd = document.querySelector(".btn");
-var inputValue = 1;
+// var inputValue = 1;
 
-function gettingValue() {
-  buttonAdd.onclick = function () {
-    inputValue = Number(input.value);
-    console.log(inputValue);
-
-    function addingElement() {
-      console.log(inputValue);
-
-      for (let item = 1; item <= inputValue; item++) {
-        const myPProd = document.createElement("div");
-        const textProd = document.createTextNode(`${item}-- textProd`);
-        console.log(textProd);
-        console.log(inputValue);
-        myPProd.appendChild(textProd);
-        myPProd.classList.add('styling-card')
-        // myPProd.appendChild(item);
-        container.appendChild(myPProd);
-      }
-    }
-    addingElement();
-  };
-}
-gettingValue();
-
-// function AddingElement() {
-//   let star = 1;
-//   let end = 10;
-//   for (let i = star; i <= end; i++) {
-//     const mydiv = document.createElement("div");
-//     const myPara = document.createElement("p");
-//     const myLink = document.createElement("a");
-//     const myTextPara = document.createTextNode(`Prod: ${i}`);
-//     const myTextlink = document.createTextNode(`Link: ${i} `);
-//     mydiv.appendChild(myTextPara);
-//     mydiv.appendChild(myPara);
-//     mydiv.appendChild(myLink);
-//     myLink.appendChild(myTextlink);
-//     myPara.appendChild(myTextPara);
-//     myLink.setAttribute("class", "link");
-//     mydiv.classList.add("styling-card");
-//     container.appendChild(mydiv);
-//   }
+// function gettingValue() {
+//   buttonAdd.onclick = function () {
+//     inputValue = parseInt(input.value);
+//     // set function
+//     function addingElement() {
+//       for (let item = 1; item <= inputValue; item++) {
+//         const myDivProd = document.createElement("div");
+//         const textProd = document.createTextNode(`${item}-- textProd`);
+//         myDivProd.appendChild(textProd);
+//         myDivProd.classList.add("styling-card");
+//         container.appendChild(myDivProd);
+//       }
+//     }
+//     addingElement();
+//   };
 // }
-// AddingElement();
+// gettingValue();
 
-// buttonAdd.onclick = function () {
-//   console.log(inputValue);
-//   inputValue = input.value;
-// };
+
